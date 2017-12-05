999bb3cc1cea9ed8e026a23f0e7d189c71eb38bd
marcoserafini2 <marcoserafini2@gmail.com>
2017-12-05 19:44:07 +0100

    Spring Initializer
    Spring Initializer

---
+++ src/main/java/it/ms/filemanager/FilemanagerApplication.java
@@ -0,0 +1,12 @@
+package it.ms.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemanagerApplication.class, args);
+	}
+}
